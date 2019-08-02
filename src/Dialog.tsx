f106bbac6353b3dcf1c3b33515e87d8add99048d
nattthebear <goyuken@gmail.com>
2019-08-02 19:38:11 -0400

    WIP build using parcel.  Vex is somehow busted.  Css @import from url doesn't work.  Babel defaults seem to be different; regenerator is required.

--- src/Dialog.tsx
+++ src/Dialog.tsx
@@ -4,16 +4,17 @@ import "vex-js/dist/css/vex.css";
 import "./Dialog.scss";
 import * as VexDialog from "vex-dialog";
 
-Vex.registerPlugin(VexDialog);
-Vex.defaultOptions.className = "vex-theme-plain";
+// Vex.registerPlugin(VexDialog);
+// Vex.defaultOptions.className = "vex-theme-plain";
 
-export function confirm(message: string) {
-	return new Promise<boolean>((resolve, reject) => {
-		Vex.dialog.confirm({
-			message,
-			callback(value: boolean) {
-				resolve(value);
-			}
-		});
-	});
+export async function confirm(message: string) {
+	return false;
+	// return new Promise<boolean>((resolve, reject) => {
+	// 	Vex.dialog.confirm({
+	// 		message,
+	// 		callback(value: boolean) {
+	// 			resolve(value);
+	// 		}
+	// 	});
+	// });
 }

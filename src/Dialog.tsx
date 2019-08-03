d2a9cd66c9b7a42f2c5f77e8b06443ac88941e34
nattthebear <goyuken@gmail.com>
2019-08-03 12:21:08 -0400

    Fix Vex; not sure who's at fault, but it's some sort of import issue.  Tweak gitignore for the devserver situation.

--- src/Dialog.tsx
+++ src/Dialog.tsx
@@ -1,20 +1,19 @@
-import * as Vex from "vex-js";
+import Vex from "vex-js";
 import "vex-js/dist/css/vex.css";
 // import "vex-js/dist/css/vex-theme-plain.css";
 import "./Dialog.scss";
-import * as VexDialog from "vex-dialog";
+import VexDialog from "vex-dialog";
 
-// Vex.registerPlugin(VexDialog);
-// Vex.defaultOptions.className = "vex-theme-plain";
+Vex.registerPlugin(VexDialog);
+Vex.defaultOptions.className = "vex-theme-plain";
 
-export async function confirm(message: string) {
-	return false;
-	// return new Promise<boolean>((resolve, reject) => {
-	// 	Vex.dialog.confirm({
-	// 		message,
-	// 		callback(value: boolean) {
-	// 			resolve(value);
-	// 		}
-	// 	});
-	// });
+export function confirm(message: string) {
+	return new Promise<boolean>((resolve, reject) => {
+		Vex.dialog.confirm({
+			message,
+			callback(value: boolean) {
+				resolve(value);
+			}
+		});
+	});
 }
